d15ec6a0338eb41c4984616146a71c92f58c1881
Ward van Asschegit config --global user.email 1060834@hr.nl <1060834@hr.nl>
2023-09-25 15:03:35 +0200

    Added bootstrap layout to exercises.blade.php

--- resources/views/exercises.blade.php
+++ resources/views/exercises.blade.php
@@ -1,13 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Exercises</title>
-</head>
-<body>
-    <h1>Exercises</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header"><h1>Exercise 1</h1></div>
+
+                    <div class="card-body">
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam non, voluptatibus. Amet aspernatur at impedit magni minus necessitatibus nemo nihil, qui repellat sequi ullam veniam? Asperiores aspernatur quasi sed sit.</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\ExercisesController@index');
+Route::get('/', function(){
+    return view('welcome');
+});
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/exercises', 'App\Http\Controllers\ExercisesController@index');
